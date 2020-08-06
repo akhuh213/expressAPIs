f1118f97d2036fd2e6d32a3a095fa5808064b1c2
Alice Aekyung Huh <aklee213@gmail.com>
2020-08-06 19:53:17 -0400

    [expressAPIs] starwars practice with CSS

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+let API_KEY = process.env.API_KEY// using .env to hide our API
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('static'))
+
+app.get('/', (req, res) => {
+    let qs = {
+        params: {
+            s: 'star wars',
+            apikey: API_KEY
+        }
+    }
+    axios.get('http://www.omdbapi.com', qs)
+    .then((response) => {
+        console.log(response.data)
+        let episodes = response.data.Search
+        // setting a variable to our data 
+        res.render('home', {episodes})
+        // render home with the data 
+    })
+
+
+    .catch(err => {
+        console.log(err)
+    })
+})
+app.listen(3000);
